11204f8fc0e25ac027173b7d418ef93731d65ebe
Antti Niemi <antti.niemi@kuuasema.com>
2018-01-28 14:34:54 +0200

    * kaik yhes koos

--- Assets/BestTimePopup.cs
+++ Assets/BestTimePopup.cs
@@ -5,12 +5,28 @@ using UnityEngine.UI;
 
 public class BestTimePopup : MonoBehaviour {
     public Text[] texts;
+    public GameObject nextPage;
+    public GameObject prevPage;
+
+    public int page = 0;
 
     void Start() {
+        nextPage.SetActive(true);
+        prevPage.SetActive(false);
         for (int i = 0; i < texts.Length; ++i) {
-            texts[i].text = "" + (i + 1) + ". " + GetMinSec(SettingsManager.Instance.GetBestTime(i + 1));
+            texts[i].text = "" + (page*4 + i + 1) + ". " + GetMinSec(SettingsManager.Instance.GetBestTime(page*4 + i + 1));
         }
     }
+
+    public void RefreshPage() {
+        page = page == 0?1:0;
+        nextPage.SetActive(page == 0);
+        prevPage.SetActive(page != 0);
+        for (int i = 0; i < texts.Length; ++i) {
+            texts[i].text = "" + (page*4 + i + 1) + ". " + GetMinSec(SettingsManager.Instance.GetBestTime(page*4 + i + 1));
+        }
+    }
+
     public string GetMinSec(float time) {
         string retVal = "";
         int minutes = (int)(time / 60f);
